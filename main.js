3712c87815ca14566e7dee8d1e4c0a82c62d5da8
Aiden-Trindade <ajef1479@gmail.com>
2026-07-26 05:41:30 +0530

    Added the mode switch button to localStorage, also made sure the text in button changes from light to dark lol, that would stay the same

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 let buttons_pressed = 0;
 
-const button = document.querySelector(".Dark-Mode");
-button.addEventListener("click",()=>{
+let mode = localStorage.getItem("theme");
+if(mode === "dark"){                                          //Checks if light or dark mode was last one//
+    document.body.classList.add("dark-mode");
+}
+
+const button1 = document.querySelector(".Dark-Mode");
+button1.addEventListener("click",()=>{
     document.body.classList.toggle("dark-mode");
+
+    const IsDark = document.body.classList.contains("dark-mode");
+    if(IsDark){
+        localStorage.setItem("theme","dark");                         //Self explanatory, boolean valuse to switch from light to dark//
+        button1.innerHTML = "Enable Light Mode";
+    }else{
+        localStorage.setItem("theme","light");
+        button1.innerHTML = "Enable Dark Mode";
+    }
+
+    
+
     buttons_pressed += 1;
     if(buttons_pressed === 15){
         alert('Keep switching and you might trigger epilepsy');
